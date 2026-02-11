1ec696437ceefbc6dc249bc88021e76a4e758d63
Nisa Najafli <ncfliniss@gmail.com>
2026-02-11 12:52:50 +0400

    new

--- assests/main.js
+++ assests/main.js
@@ -15,3 +15,39 @@ document.querySelector('.hero-prev').addEventListener('click', () => {
   index = (index - 1 + slides.length) % slides.length;
   updateSlider();
 });
+
+
+
+const items = document.querySelectorAll(".faq-item");
+
+  items.forEach(item => {
+    const btn = item.querySelector(".faq-question");
+    const answer = item.querySelector(".faq-answer");
+
+    btn.addEventListener("click", () => {
+      const isOpen = item.classList.contains("active");
+
+      // close all
+      items.forEach(i => {
+        i.classList.remove("active");
+        i.querySelector(".faq-answer").style.height = 0;
+      });
+
+      if (!isOpen) {
+        item.classList.add("active");
+        answer.style.height = answer.scrollHeight + "px";
+      }
+    });
+  });
+
+  const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 200) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+document.getElementById("year").textContent = new Date().getFullYear();
